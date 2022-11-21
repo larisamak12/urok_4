71859da323b32b88145467393d0cc47dd69e6572
Larisa <larisamak12@mail.ru>
2022-11-21 12:51:14 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine());
+
+int Func (int number)
+{
+    int sum = 0;
+    while (number>0)
+    {
+        sum = sum + number%10;
+        number=number/10;
+    }
+    return sum;
+}
+Console.WriteLine("Сумма всех цифр рана" + Func (number));
